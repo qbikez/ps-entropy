1345d0995739e2f143ba1f2af1d68ebe8ead4b2a
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-05-16 07:38:30 +0200

    add newtonsoft.json module

---
+++ src/newtonsoft.json/newtonsoft.json.psm1
@@ -0,0 +1,38 @@
+$asm = [Reflection.Assembly]::LoadFile("$PSScriptRoot\lib\Newtonsoft.Json.dll")
+
+function ConvertFrom-JObject($obj) {
+   if ($obj -is [Newtonsoft.Json.Linq.JArray]) {
+        $a = @()
+        foreach($entry in $obj.GetEnumerator()) {
+            $a += @(convertfrom-jobject $entry)
+        }
+        return $a
+   }
+   elseif ($obj -is [Newtonsoft.Json.Linq.JObject]) {
+       $h = [ordered]@{}
+       foreach($kvp in $obj.GetEnumerator()) {
+            $val =  convertfrom-jobject $kvp.value
+            if ($kvp.value -is [Newtonsoft.Json.Linq.JArray]) { $val = @($val) }
+            $h += @{ "$($kvp.key)" = $val }
+       }
+       return $h
+   }
+   elseif ($obj -is [Newtonsoft.Json.Linq.JValue]) {
+        return $obj.Value
+   }
+   else {
+    return $obj
+   }
+}
+
+function ConvertFrom-JsonNewtonsoft([Parameter(Mandatory=$true,ValueFromPipeline=$true)]$string) {
+	$obj = [Newtonsoft.Json.JsonConvert]::DeserializeObject($string, [Newtonsoft.Json.Linq.JObject])
+    
+    return ConvertFrom-JObject $obj
+}
+
+function ConvertTo-JsonNewtonsoft([Parameter(Mandatory=$true,ValueFromPipeline=$true)]$obj) {
+    return [Newtonsoft.Json.JsonConvert]::SerializeObject($obj, [Newtonsoft.Json.Formatting]::Indented)
+}
+
+Export-ModuleMember -Function ConvertFrom-JsonNewtonsoft,ConvertTo-JsonNewtonsoft
