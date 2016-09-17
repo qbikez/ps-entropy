6eb0520b88497031a3596cfa834aaeef65ff71bc
jakub.pawlowski <jakub.pawlowski@legimi.com>
2016-09-17 15:48:44 +0200

    add stack, idea, todo to oneliners

--- src/oneliners/oneliners.psm1
+++ src/oneliners/oneliners.psm1
@@ -327,6 +327,169 @@ function enable-hyperv {
     write-host "shutdown /r /t 0 /f"
 }
 
+ipmo cache -ErrorAction Ignore
+
+function get-syncdir() {
+    if (test-path "HKCU:\Software\Microsoft\OneDrive") 
+    {
+        $prop = get-itemproperty "HKCU:\Software\Microsoft\OneDrive\" "UserFolder"
+        if ($prop -ne $null) {
+            $dir = $prop.userfolder
+        }        
+    }
+
+    if ($dir -ne $null) {
+        $syncdir = join-path $dir ".powershell-data"
+        if (!(test-path $syncdir)) {
+            $null = mkdir $syncdir
+        }
+        return $syncdir
+    }
+}
+
+function push {
+    param([Parameter(Mandatory=$true,ValueFromPipeline=$true)] $what, $stackname = "default") 
+
+    $stack = import-cache -container "stack.$stackname" -dir (get-syncdir)
+    
+    if ($stack -eq $null) { $stack = @(); $no = 1 }
+    else { $stack = @($stack); $no = $stack.Length + 1 }
+
+    $props = [ordered]@{
+        no = $no
+        value = $what
+        ts = get-date -Format "yyyy-MM-dd HH:mm:ss"
+    }
+    $item = new-object -type pscustomobject -Property $props
+    $stack += @($item)
+    export-cache -data $stack -container "stack.$stackname"  -dir (get-syncdir)
+    peek -stackname $stackname
+}
+
+function pop {
+    param($stackname = "default") 
+    
+    $stack = import-cache -container "stack.$stackname" -dir (get-syncdir)
+    if ($stack -eq $null -or $stack.length -eq 0) { return $null }
+    else { $stack = @($stack) }
+    $item = $stack[$stack.length-1]
+    $stack = $stack | select -First ($stack.Length-1)
+    if ($stack -eq $null) {
+        remove-stack -container "stack.$stackname" -dir (get-syncdir)
+    } else {
+        export-cache -data $stack -container "stack.$stackname" -dir (get-syncdir)
+    }
+    return $item
+}
+
+function peek {
+    param($stackname = "default") 
+
+    $stack = @(import-cache -container "stack.$stackname" -dir (get-syncdir))
+    if ($stack -eq $null -or $stack.length -eq 0) { return $null }
+    $item = $stack[$stack.length-1]
+    return $item
+}
+
+function get-stack {
+    param($stackname = "default") 
+
+    $stack = import-cache -container "stack.$stackname" -dir (get-syncdir)
+    return $stack
+}
+
+
+function remove-stack {
+    param($stackname = "default") 
+
+    remove-cache -container "stack.$stackname" -dir (get-syncdir)    
+}
+
+function idea {
+    [Cmdletbinding(DefaultParameterSetName="list")]
+    param(
+        [Parameter(mandatory=$true,ParameterSetName="add",Position=1)]
+        $idea,                 
+        [Parameter(mandatory=$true,ParameterSetName="search")]
+        $search,
+        [Parameter(mandatory=$false,ParameterSetName="add")]
+        [switch][bool]$go,
+        [Parameter(mandatory=$false,ParameterSetName="add")]
+        [Parameter(mandatory=$false,ParameterSetName="list")]
+        [switch][bool]$done,
+        [Parameter(mandatory=$false,ParameterSetName="add")]
+        [switch][bool]$remove,
+        [Parameter(mandatory=$false)]$stackname = "ideas"
+    ) 
+    switch($PSCmdlet.ParameterSetName) {
+        { $_ -eq "add" -and !$done -and !$remove } {             
+            if ($go) {
+                if ($idea.gettype() -eq [int]) {
+                    $found = idea -search $idea
+                    if ($found -eq $null) { return }
+                    $idea = $found
+                }                 
+                else {
+                    push $idea -stackname $stackname
+                }
+                push "idea: $($idea.value)"
+            } else {
+                push $idea -stackname $stackname
+            }
+        }
+        "list" {
+            if ($done) {
+                stack -stackname "$stackname.done"    
+            } else {
+                stack -stackname $stackname    
+            }
+        }
+        { $_ -eq "search" `
+            -or ($_ -eq "add" -and ($done -or $remove)) } {
+            $ideas = stack -stackname $stackname  
+            if ($search -eq $null) { $search = $idea } 
+            $found = $ideas | ? { (($search.gettype() -eq [int]) -and $_.no -eq $search) -or $_.value -match "$search" }
+            if ($found -eq $null) {
+                if ($search.gettype() -eq [int]) { write-warning "no idea with id $search found" }
+                else { write-warning "no idea matching '$search' found" }
+                return
+            }
+            $found = @($found) 
+
+            if ($_ -eq "search") {
+                return $found
+            }
+
+            if ($found.Length -gt 1) {
+                write-warning "more than one idea matching '$search' found:"
+                $found | format-table | out-string | write-host
+                return
+            }                        
+            write-verbose "found matching idea: $found" 
+            
+            if ($done) {
+                push $found[0] -stackname "$stackname.done"
+            }
+            if ($done -or $remove) {
+                $newstack = $ideas | ? { $_.no -ne $found[0].no }
+                export-cache -data $newstack -container "stack.$stackname" -dir (get-syncdir)            
+            }
+        }
+    }    
+}
+
+function pop-idea {
+     pop -stackname "ideas"
+}
+
+
+function todo {
+    param([Parameter(mandatory=$false)]$what)
+
+    idea $what -stackname "todo"
+}
+
+new-alias stack get-stack
 new-alias tp test-path
 new-alias git-each invoke-giteach
 new-alias gitr git-each
