d41fbda6dbe8e4e598cf50d13ef3af1b4312b0d2
jakub.pawlowski <jakub.pawlowski@legimi.com>
2017-03-21 07:58:41 +0100

    oneliners: update send-slack command

---
+++ src/Require/Untitled1.ps1
@@ -0,0 +1,16 @@
+﻿function foo {
+ trap {
+    write-host "dupa: $_"   
+    continue 
+ }
+ throw "I'm an exception"
+ Write-Error -Exception "written exception" -ErrorAction stop
+ 
+ write-host "after throw"
+}
+
+foo
+
+write-host "after foo"
+
+Write-Error "a" -ErrorAction stop
