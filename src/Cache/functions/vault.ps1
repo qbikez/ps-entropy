7de51e9aa25fa6f1bf330546e09f0bd01fead31f
jakub.pawlowski <pawlowskijakub@gmail.com>
2017-10-23 11:17:11 +0200

    better vault support, including vault auth

--- src/Cache/functions/vault.ps1
+++ src/Cache/functions/vault.ps1
@@ -3,10 +3,30 @@ function Export-VaultCache([Parameter(Mandatory=$true,ValueFromPipeline=$true)]$
 }
 
 function Import-VaultCache([Parameter(Mandatory=$true)]$container) {    
-    $json = invoke vault read "-format=json" $container -passthru -showoutput:$false -nothrow -passerrorstream | out-string
-    if ($lastexitcode -ne 0) {
-        throw "vault read failed: $json"
-    }
-    $data = ConvertFrom-Json $json
-    return $data.data
+    $retry = $true
+    do {
+        $json = invoke vault read "-format=json" $container -passthru -showoutput:$false -nothrow -passerrorstream | out-string
+        if ($lastexitcode -ne 0) {
+        if ($json -match "Code:\s*([0-9]+)") {
+            $code = $matches[1]
+            if ($code -eq "403") {
+                # authorize in vault using stored credentials
+                $vaultCredentials = get-credentialscached -container "vault"
+                if ($vaultCredentials -ne $null) {
+                    $r = invoke vault auth "--method=ldap" "username=($vaultCredentials.username)" "password=($vaultcredentials.getnetworkcredential().password)"
+                    continue
+                }
+                
+                throw "vault read returned 403, and vault auth failed"
+            }
+        }
+            throw "vault read failed: $json"
+        }
+        # credentials.ps1 expects password in form of encoded securestring
+        $data = ConvertFrom-Json $json
+        if ($data.password -ne $null) {
+            $data.password = converfrom-securestring (convertto-securestring $data.password -force -AsPlainText)
+        }        
+        return $data.data
+    } while ($retry)
 }
