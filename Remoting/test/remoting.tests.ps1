25c8ff5ce784f17f7c81aaea9683cee4e8d0bdea
jakub.pawlowski <pawlowskijakub@gmail.com>
2018-10-30 07:12:35 +0100

    remoting: create missing target dirs on remote machine

--- Remoting/test/remoting.tests.ps1
+++ Remoting/test/remoting.tests.ps1
@@ -3,9 +3,19 @@ if (gmo Remoting) {
 }
 
 ipmo $PSScriptRoot\..\src\Remoting.psd1
+$server = "docker/iis"
 
 Describe "Remoting module tests" {
-    It "should copy files recursively" {
-        $session = rps localhost -noenter
+    $target = "c:\test\"
+    It "should copy files recursively" -Pending {
+        $session = rps $server -noenter
+        invoke-command -Session $session -scriptBlock { if (test-path $using:target) { rmdir $using:target -Recurse -force } }
+
+        Copy-RemoteFile -source "$PSScriptRoot/input" -target "$target" -tosession $session -Verbose
+
+        $listing = invoke-command -Session $session -scriptBlock { ls $using:target -Recurse }
+        $listing | format-table | out-string | write-host
+
+        $listing.count | Should -Be 3
     }
 }
